1e70c3e34c6c33bc718532f64581b9538c4cfdd4
Atif <iamatif96@gmail.com>
2024-10-19 10:39:36 +0800

    package for logging and test fixed

--- src/test/java/com/atif/CompleteProject/e2e/CompleteProjectE2ETest.java
+++ src/test/java/com/atif/CompleteProject/e2e/CompleteProjectE2ETest.java
@@ -12,7 +12,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class UserCrudE2ETest {
+public class CompleteProjectE2ETest {
 
     @Autowired
     private MockMvc mockMvc;
